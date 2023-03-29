570d04f64810f2c5620209126b7da76d846fae28
harun yardimci <harun.yardimci@deliveryhero.com>
2023-03-29 12:55:18 +0200

    Update json-formatter.py

--- json-formatter.py
+++ json-formatter.py
@@ -1,5 +1,6 @@
 import json
-import sys
+import argparse
+import os
 
 def format_json(json_data):
     try:
@@ -8,21 +9,19 @@ def format_json(json_data):
     except ValueError as e:
         return "Unable to format as JSON: " + str(e)
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='JSON Formatter.')
+parser.add_argument('json_input', metavar='<json_input>', type=str,
+                    help='the JSON file or string to format')
+args = parser.parse_args()
+
 # Get JSON data from either a file or a command line argument
-if len(sys.argv) == 1:
-    print("Please provide a filename or JSON string as an argument.")
-    sys.exit()
-elif len(sys.argv) == 2:
-    try:
-        with open(sys.argv[1], 'r') as f:
-            json_data = f.read()
-    except FileNotFoundError as e:
-        json_data = sys.argv[1]
+if os.path.isfile(args.json_input):
+    with open(args.json_input, 'r') as f:
+        json_data = f.read()
 else:
-    print("Please provide only one filename or JSON string as an argument.")
-    sys.exit()
+    json_data = args.json_input
 
 # Format the JSON data and print the result
 formatted = format_json(json_data)
 print(formatted)
-
